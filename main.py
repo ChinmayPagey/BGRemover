2ff75fa2cffff66d212415636084ac032088a26d
ChinmayPagey <pageychinmay@gmail.com>
2024-12-12 16:28:30 +0530

    Use PORT enviroment variable for dynamic port binding

--- main.py
+++ main.py
@@ -71,4 +71,5 @@ async def remove_background(request: ImageProcessRequest):
 # Run the app if executed directly
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
